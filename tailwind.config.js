f58f04e1ae5d0f218d6bebbb16492268254395bf
Douglas-Marques <douglasmarques09@gmail.com>
2021-03-14 20:11:59 -0300

    End of tutorial

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,22 @@
 module.exports = {
-  purge: [],
+  purge: [
+    "./src/**/*.jsx", "./index.html"
+  ],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        brand: {
+          DEFAULT: "#0fa9e6",
+          light: "#3fbaeb",
+          dark: "#0c87b8"
+
+        }
+      },
+      fontFamily: {
+        headline: "Poppins, sans-serif"
+      }
+    },
   },
   variants: {
     extend: {
